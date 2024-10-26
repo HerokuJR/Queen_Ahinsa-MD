985470b437aa8ebb1f53eb2d650ce5ecd4e37133
HerokuSL <ddilisha78@gmail.com>
2024-10-26 10:09:00 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -18,7 +18,7 @@ let status = `
 ┃▢│  Uptime    ➤  ${runtime(process.uptime())}
 ┃▢│  Ram usage ➤  ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 ┃▢│  HostName  ➤  ${os.hostname()}
-┃▢│  Owner     ➤  ${process.Owner()}    
+┃▢│  Owner     ➤  Dilisha  
 ┃▢│  Contact   ➤  0775713391 
 ┃▢│  TEAM OF   ➤  𝐹𝐴𝑀𝐼𝐿𝑌 𝐵𝑂𝑇𝑍 🎬
 ┃▢╰─────────────┈
