cd274484d7cf45f340e2fa0cc694dbda7498d156
HerokuSL <ddilisha78@gmail.com>
2024-10-27 10:12:44 +0530

    Update ytmp3.js

--- plugins/ytmp3.js
+++ plugins/ytmp3.js
@@ -1,58 +1,49 @@
-// MEDIAFIRE DOWNLOAD COMMAND
+// YT MP3 DOWNLOAD COMMAND 
 
+const { cmd } = require('../command')
 const { fetchJson } = require('../lib/functions')
-const config = require('../config')
-const { cmd, commands } = require('../command')
 
-const apilink = 'https://api-pink-venom.vercel.app/api/ytmp3?url=https://youtube.com/watch?v=05dpQG1FcfU' // API LINK ( DO NOT CHANGE THIS!! )
-
+const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
 
 cmd({
-    pattern: "audio",
-    alias: ["audio","mp3,yta"],
-    desc: "audio",
-    category: "audio download",
-    use: '.audio < audio url >',
+    pattern: "song",
+    desc: "download songs.",
+    category: "download",
+    react: "🎧",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, reply, q }) => {
+async(conn, mek, m,{from, reply, q}) => {
 try{
-  
-if(!q) return await reply("Please give me mediafire url");
-  if(!q.includes('youtube.com')) return await reply("This url is invalid");
-  
-const mfire = await fetchJson(`${apilink}/download/audio?url=${q}`);
-  
-const msg = `
-           🔥 *AUDIO DOWNLOADER* 🔥
 
-  
-    ╭════ 🅼🆄🆂🅸🅲 ════─❃
-┃▢╭─────────────┈
-┃▢│ tittle: ${data.tittle}
-┃▢│ despcription: ${data.description} 
-┃▢│ time: ${data.time}
-┃▢| ago: ${data.ago}     
-┃▢│ views: ${data.views}
-┃▢│ like: ${data.like}
-┃▢╰─────────────┈
-╰══════════════════─❃
+if(!q) return reply('Give me song name or url !')
+    
+const search = await fetchJson(`${apilink}/search/yt?q=${q}`)
+const data = search.result.data[0];
+const url = data.url
+    
+const ytdl = await fetchJson(`${apilink}/download/ytmp3?url=${data.url}`)
+    
+let message = `‎‎           🎶 YT SONG DOWNLOADER 🎶
 
+
+ 🎵 ‎Title: ${data.title}
+ ⏱ Duration: ${data.timestamp}
+ 🌏 Uploaded: ${data.ago}
+ 🧿 Views: ${data.views}
+ 🤵 Author: ${data.author.name}
+  📎 Url: ${data.url}
 `
   
-// SEND DETAILS
-
-await conn.sendMessage(from,{image:{url: data. thumbnail},caption:desc},{quoted:mek});
-
-// SEND FILE
-
-await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"𝑨𝑼𝑫𝑰𝑶 🎵"},{quoted:mek})
-
-
-} catch (e) {
+await conn.sendMessage(from, { image: { url : data.thumbnail }, caption: message }, { quoted : mek })
+  
+// SEND AUDIO NORMAL TYPE and DOCUMENT TYPE
+await conn.sendMessage(from, { audio: { url: ytdl.result.dl_link }, mimetype: "audio/mpeg" }, { quoted: mek })
+await conn.sendMessage(from, { document: { url: ytdl.result.dl_link }, mimetype: "audio/mpeg", fileName: data.title + ".mp3", caption: `${data.title}`}, { quoted: mek })
+  
+} catch(e){
 console.log(e)
-reply('This url type is not working !!')
+reply(e)
 }
 })
 
+// FOLLOW US : https://whatsapp.com/channel/0029VaaPfFK7Noa8nI8zGg27
