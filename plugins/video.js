864a3ced6d43bcffaba8f1c6dd5753920a35923e
HerokuSL <ddilisha78@gmail.com>
2024-10-27 17:01:18 +0530

    Update video.js

--- plugins/video.js
+++ plugins/video.js
@@ -3,7 +3,7 @@
 const { cmd } = require('../command')
 const { fetchJson } = require('../lib/functions')
 
-const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
+const apilink = 'https://tinyurl.com/2axc42rt' // API LINK ( DO NOT CHANGE THIS!! )
 
 cmd({
     pattern: "video",
