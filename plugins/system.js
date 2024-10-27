63d316c8bac45cd3d90c22717fde3e3a91497323
HerokuSL <ddilisha78@gmail.com>
2024-10-27 08:13:52 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -13,12 +13,15 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let status = `
-      Uptime:   ➤   ${runtime(process.uptime())}
-      Ram usage: ➤  ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-      HostName:  ➤  ${os.hostname()}
-      Owner:     ➤  Dilisha  
-      Contact:   ➤  0775713391 
-      TEAM OF:   ➤  𝐹𝐴𝑀𝐼𝐿𝑌 𝐵𝑂𝑇𝑍 🎬
+_________________________________________
+
+*Uptime*: ${runtime(process.uptime())}
+*Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*HostName*: ${os.hostname()}
+*Owner*: Dilisha gimshan
+*Contact*: 0775713391
+*TEAM OF*: FAMILY BOTS
+_____________________________________________
 `
 
 return reply(`${status}`)
