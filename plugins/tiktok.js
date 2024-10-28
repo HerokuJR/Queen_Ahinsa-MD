8eb1afc028de1e0b1a9e0cd7a443fedb9c472ade
HerokuSL <ddilisha78@gmail.com>
2024-10-28 08:50:26 +0530

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -43,15 +43,10 @@ const msg = `
 // SEND DETAILS
 await conn.sendMessage( from, { image: { url: tiktok.result.cover || '' }, caption: msg }, { quoted: mek });
 
-// SEND WATER MARK VIDEO
-await conn.sendMessage(from, { video: { url: tiktok.result.wmVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nWATERMARK VIDEO ✅` }, { quoted: mek });
-  
+
 // SEND HD VIDEO
 await conn.sendMessage(from, { video: { url: tiktok.result.hdVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nNO-WATERMARK VIDEO ✅` }, { quoted: mek });
-  
-// SEND AUDIO
-await conn.sendMessage(from, { audio: { url: tiktok.result.sound }, mimetype: "audio/mpeg" }, { quoted: mek });
-
+await m.react("✅");
   
 } catch (e) {
 console.log(e)
