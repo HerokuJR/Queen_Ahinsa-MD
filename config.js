4b719c770625d59d10e9c5c055e9c54756f5802b
HerokuSL <ddilisha78@gmail.com>
2024-10-28 11:08:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Fu93DSAS#REOYweYsbhX5Jwa2X5vrht_WQpKrReGGeZEg5olWZJQ",
-MONGODB: process.env.MONGODB || "mongodb://${{MONGO_INITDB_ROOT_USERNAME}}:${{MONGO_INITDB_ROOT_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}",
+MONGODB: process.env.MONGODB || "mongodb+srv://whatsappbot11000:<db_password>@cluster0.hbfdf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 
 };
