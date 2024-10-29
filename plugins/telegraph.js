95d706b8d931854bd10487a8eca2611578b3d25f
HerokuSL <ddilisha78@gmail.com>
2024-10-29 19:02:36 +0530

    Update telegraph.js

--- plugins/telegraph.js
+++ plugins/telegraph.js
@@ -1,4 +1,6 @@
-const { cmd } = require('../command');
+const { fetchJson } = require('../lib/functions')
+const config = require('../config')
+const { cmd, commands } = require('../command')
 const axios = require('axios');
 const util = require('util');
 const { Buffer } = require('buffer');
