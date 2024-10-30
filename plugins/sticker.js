5bcd2cee6521bcec1bb134be77694275df6b89a7
HerokuSL <ddilisha78@gmail.com>
2024-10-30 07:54:52 +0530

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -9,7 +9,7 @@ else imgmsg = 'ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ ғᴏʀ sᴛɪᴄᴋᴇʀ!
 
 var descg = '';
 if (config.LANG === 'SI') descg = 'එය ඔබගේ mention දුන් ඡායාරූපය ස්ටිකර් බවට පරිවර්තනය කරයි.';
-else descg = 'ɪᴛ ᴄᴏɴᴠᴇʀᴛs ʏᴏᴜʀ ʀᴇᴘʟɪᴇᴅ ᴘʜᴏᴛᴏ ᴛᴏ sᴛɪᴄᴋᴇʀ.';
+else descg = 'WHATSAPP BOTZ STICKER BY Queen_Ahinsa-MD.';
 
 cmd({
     pattern: 'sticker',
