27fd677bd0dda5496dd77608454dd8d183f55ec7
HerokuJR <dilishagimshan636@gmail.com>
2024-11-15 07:28:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:f4babd886f8d3c5548a5f4fd041f4d68",
-MONGODB: process.env.MONGODB || "mongodb://mongo:xAspwrXVpAyKRvrWGnucEnWyLTihRwsi@junction.proxy.rlwy.net:11262",
+SESSION_ID: process.env.SESSION_ID || "Fu93DSAS#REOYweYsbhX5Jwa2X5vrht_WQpKrReGGeZEg5olWZJQ",
+MONGODB: process.env.MONGODB || "mongodb://mongo:UxVIjEDkBxCySBsnbzPYQBebAmxvzbjg@junction.proxy.rlwy.net:17429",
 
 };
